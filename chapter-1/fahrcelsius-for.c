664641a82aca1919595dbe219d8a462b576a0961
fremydev <fremy.dev@gmail.com>
2023-05-28 11:55:29 -0300

    use macros instead of magic numbers

--- chapter-1/fahrcelsius-for.c
+++ chapter-1/fahrcelsius-for.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+#define FAHR_TO_CELSIUS(fahr) ((5.0/9.0) * (fahr-32))
+
 /* Fahrenheit to Celsius table using for loop */
 main () {
 	int fahr;
 
-	for (fahr = 0; fahr <=300; fahr = fahr + 20)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	for (fahr = LOWER; fahr <=UPPER; fahr = fahr + STEP)
+		printf("%3d %6.1f\n", fahr, FAHR_TO_CELSIUS(fahr));
 }
