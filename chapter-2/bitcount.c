8dc7f13759b8b147c9f7c7ae862ab88c21fa1755
fremydev <fremy.dev@gmail.com>
2023-06-17 16:30:41 -0300

    adds bitcount program

---
+++ chapter-2/bitcount.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int bitcount(unsigned x);
+
+int main(void) {
+    return 0;
+}
+
+int bitcount(unsigned x) {
+    int b;
+
+    for (b = 0; x != 0; x >>= 1)
+        if (x & 01)
+            b++;
+    return b;
+}
